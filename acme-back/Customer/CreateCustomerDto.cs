c6254fe5e912b8dbb3901a586cd22000423c6472
Raul <you@example.com>
2024-06-05 10:25:10 +0200

    add Customer migrations

---
+++ acme-back/Customer/CreateCustomerDto.cs
@@ -0,0 +1,7 @@
+namespace acme_back.Customer;
+
+public class CreateCustomerDto
+{
+    public string Name { get; set; } = null!;
+    public DateTime EntryDate { get; set; } 
+}
--- acme-back/Customer/CustomerController.cs
+++ acme-back/Customer/CustomerController.cs
@@ -1,5 +1,7 @@
+using acme_back.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace acme_back.Customer
 {
@@ -7,5 +9,32 @@ namespace acme_back.Customer
     [ApiController]
     public class CustomerController : ControllerBase
     {
+        private readonly ApplicationDBContext _context;
+
+        public CustomerController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<CustomerDto>>> GetAllCustomer()
+        {
+            var customer = await _context.Customers.ToListAsync();
+
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerDto>> CreateCustomer(CreateCustomerDto createCustomerDto)
+        {
+            var customer = new Customer
+            {
+                Name = createCustomerDto.Name,
+                EntryDate = createCustomerDto.EntryDate
+            };
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+            return Ok(customer);
+        }
     }
 }
